47f6938c3f36e2811201e52e731d83b771708945
Arham Choudhury <arham.choudhury@gmail.com>
2022-08-01 20:50:03 -0500

    new post

--- git_script.R
+++ git_script.R
@@ -4,6 +4,13 @@ library(usethis)
 use_git()
 use_github()
 
-
+distill::create_post(
+  title = "inflation",
+  author = "auto",
+  slug = "auto", # generates a website slug (URL)
+  date_prefix = TRUE, # adds date for sorting
+  draft = FALSE,
+  edit = interactive()
+)
 
 # end
